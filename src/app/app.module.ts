430c8be9ab7d865e2cc42f867f4563427dce19a6
RodrigoBessa85 <rbessa_analista@hotmail.com>
2022-04-21 17:33:21 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SingupComponent } from './singup/singup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SingupComponent
   ],
   imports: [
     BrowserModule
